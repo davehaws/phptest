00bb1d7b270b75b4d576ef76e8878ab6ed93ff9e
Dave Haws <dhaws@decision-wise.com>
2015-06-04 22:30:17 -0600

    Probably could have been a bit faster. As it is, I am allocating arrays everywhere. But I didn't want to deal with all the math that would have made it more difficult. Could also have made some improvements with passing things around by reference, but I have been bitten by that before and try to avoid references expect where absolutely necessary. Could also have built in static buffers rather than calculating the buffers along the way.

--- tasks/003/lib/Pascal.php
+++ tasks/003/lib/Pascal.php
@@ -1,4 +1,96 @@
 <?php
+
 class Pascal {
 
+    private $levels;
+
+    public function __construct($levels) {
+        if (!is_int($levels) || $levels < 1) {
+            throw new InvalidArgumentException("Must pass in a positive integer. '$levels' is not valid.");
+        }
+        $this->levels = $levels;
+    }
+
+    private function getNextPascalLevel(array $previous) {
+        $count = count($previous);
+
+        $elements = [];
+        $max = 0;
+        for ($i = 0; $i < $count+1; $i++) {
+            // First and last element will be one
+            if ($i == 0 || $i == $count) {
+                $int = 1;
+            } else {
+                // Otherwise, add together the two integers above
+                $int = $previous[$i-1] + $previous[$i];
+            }
+            $elements[] = $int;
+            if ($int > $max) {
+                $max = $int;
+            }
+        }
+        return [$elements, $max];
+    }
+
+    private function getPascalIntegerArraysAndMax() {
+        $all_arrays = [];
+        $elements = [];
+        $max_num = 0;
+        for ($i = 0; $i < $this->levels; $i++) {
+            $last_level = count($all_arrays) ? $all_arrays[$i-1] : [];
+            list($elements, $max_num) = $this->getNextPascalLevel($last_level);
+
+            $all_arrays[] = $elements;
+        }
+
+        return [$all_arrays, $max_num];
+    }
+
+    private function convertLevelArrayToString(array $level, $int_len) {
+        $strings = [];
+        foreach ($level as $int) {
+             $strings[] = str_pad($int, $int_len, ' ', STR_PAD_BOTH);
+        }
+
+        // We buffered everything, but last number can't have spaces to the right
+        $last_index = count($strings) - 1;
+        $strings[$last_index] = rtrim($strings[$last_index]);
+
+        $buffer = str_pad('', $int_len);
+        return implode($buffer, $strings);
+    }
+
+    private function convertLevelArraysToStrings(array $all_levels, $int_len) {
+        $strings = [];
+        foreach ($all_levels as $level) {
+            $strings[] = $this->convertLevelArrayToString($level, $int_len);
+        }
+        return $strings;
+    }
+
+    private function centerStrings(array $strings, $int_len) {
+        $centered_strings = [];
+
+        // Start with most amount of padding we need, then will take less and less of it
+        $padding = str_pad('', ($this->levels - 1)*$int_len);
+
+        foreach ($strings as $string) {
+            $centered_strings[] = $padding . $string;
+            $padding = substr($padding, 0, -1*$int_len);
+        }
+        return $centered_strings;
+    }
+
+    public function output() {
+        list($all_levels, $max_int) = $this->getPascalIntegerArraysAndMax();
+        $int_len = strlen("$max_int");
+
+        $level_strings = $this->convertLevelArraysToStrings($all_levels, $int_len);
+
+        $centered_strings = $this->centerStrings($level_strings, $int_len);
+
+        $triangle_string = implode("\r\n", $centered_strings);
+
+        return $triangle_string;
+    }
 }
--- tasks/003/test/PascalTest.php
+++ tasks/003/test/PascalTest.php
@@ -1,33 +1,81 @@
 <?php
-require_once('lib/Pascal.php');
+
+require_once(dirname(__FILE__).'/../lib/Pascal.php');
+
 class PascalTest extends PHPUnit_Framework_TestCase {
-  function testSmallTriangle(){
-    $expected = <<<EOD
-    1    
-   1 1   
-  1 2 1  
- 1 3 3 1 
+
+    public function testInitialTringle() {
+        $expected = <<<EOD
+1
+EOD;
+
+        $pascal = new Pascal(1);
+        $this->assertEquals($pascal->output(), $expected);
+    }
+
+    public function testTwoLevelTringle() {
+        $expected = <<<EOD
+ 1
+1 1
+EOD;
+
+        $pascal = new Pascal(2);
+        $this->assertEquals($pascal->output(), $expected);
+    }
+
+    public function testFirstTringleWithItemsGreaterThanOne() {
+        $expected = <<<EOD
+  1
+ 1 1
+1 2 1
+EOD;
+
+        $pascal = new Pascal(3);
+        $this->assertEquals($pascal->output(), $expected);
+    }
+
+    function testSmallTriangle(){
+        $expected = <<<EOD
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
 1 4 6 4 1
 EOD;
 
-    $pascal = new Pascal(5);
-    $this->assertEquals($pascal->output(), $expected);
-  }
+        $pascal = new Pascal(5);
+        $this->assertEquals($pascal->output(), $expected);
+    }
 
-  function testBigTriangle(){
-    $expected = <<<EOD
-                            1                            
-                         1     1                         
-                      1     2     1                      
-                   1     3     3     1                   
-                1     4     6     4     1                
-             1     5    10    10     5     1             
-          1     6    15    20    15     6     1          
-       1     7    21    35    35    21     7     1       
-    1     8    28    56    70    56    28     8     1    
- 1     9    36    84    126   126   84    36     9     1 
+    function testFirstTwoDigitTriangle(){
+        $expected = <<<EOD
+          1
+        1   1
+      1   2   1
+    1   3   3   1
+  1   4   6   4   1
+1   5   10  10  5   1
 EOD;
-    $pascal = new Pascal(10);
-    $this->assertEquals($pascal->output(), $expected);
-  }
+
+        $pascal = new Pascal(6);
+        $this->assertEquals($pascal->output(), $expected);
+    }
+
+    function testBigTriangle(){
+        $expected = <<<EOD
+                            1
+                         1     1
+                      1     2     1
+                   1     3     3     1
+                1     4     6     4     1
+             1     5    10    10     5     1
+          1     6    15    20    15     6     1
+       1     7    21    35    35    21     7     1
+    1     8    28    56    70    56    28     8     1
+ 1     9    36    84    126   126   84    36     9     1
+EOD;
+
+        $pascal = new Pascal(10);
+        $this->assertEquals($pascal->output(), $expected);
+    }
 }
